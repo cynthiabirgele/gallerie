20107d5e9181ca598e1068d50c8b36f87069641d
Sintija <sintija.birgele@avanade.com>
2018-07-26 18:03:42 +0300

    update index

--- gallerie/src/App.js
+++ gallerie/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react"
 import "./App.css"
 import Header from "./components/organisms/header/header"
 import Navigation from "./components/organisms/navigation/navigation"
+import Home from "./components/pages/Home/Home"
 
 class App extends Component {
 	render() {
+		const isHome = this.props.location.pathname.length <= 1 || ``
 		return (
 			<div className="container">
 				<Header />
 				<Navigation />
+				{isHome && <Home />}
 			</div>
 		)
 	}
